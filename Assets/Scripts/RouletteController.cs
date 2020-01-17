52f0bb53f45bae5957dcf53a8928b2022882a869
hiroto302 <i.am.a.hiroto@gmail.com>
2020-01-17 23:07:49 +0900

    AddScecondSpeedFunction
    １度目のタップで回転し、２度目のタップで減速する仕様に変更

--- Assets/Scripts/RouletteController.cs
+++ Assets/Scripts/RouletteController.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// ルーレットオブジェクトにはこちらのスクリプトをアタッチ
 public class RouletteController : MonoBehaviour
 {
-    float rotSpeed = 0; //回転スピード
+    float firstRotSpeed = 0; //１度目の回転速度変数
+    float secondRotSpeed= 0; //２度目のタップでの回転速度変数
+
 
     void Start()
     {
@@ -13,55 +16,62 @@ public class RouletteController : MonoBehaviour
 
     void Update()
     {
-
-        //タップの制限
-        if(this.rotSpeed == 0)
+        if( this.firstRotSpeed == 10.0f )
         {
-            //回転機能
-            if(Input.GetMouseButton(0))
+            //2回目のタップで回転の減速
+            if(Input.GetMouseButtonDown(0))
             {
-                this.rotSpeed = 10.0f;
+                this.firstRotSpeed = 0;
+                this.secondRotSpeed = 10.0f;
+                Debug.Log("2");
             }
         }
-        else
+        //タップの制限
+        else if(this.firstRotSpeed == 0)
         {
-            transform.Rotate(0, 0, this.rotSpeed);
-            this.rotSpeed *= 0.98f;
-            if(this.rotSpeed < 0.05f)
+            //回転機能
+            if(Input.GetMouseButtonDown(0))
             {
-                this.rotSpeed =0;
+                this.firstRotSpeed = 10.0f;
+                Debug.Log("1");
+            }
+        }
+
+        transform.Rotate(0, 0, this.firstRotSpeed + secondRotSpeed);
+        this.secondRotSpeed *= 0.98f;
+
+        //２度目のタップで止まる時、くじ結果を表示
+        if( 0 < this.secondRotSpeed )
+        {
+            if(this.secondRotSpeed < 0.05f  && firstRotSpeed == 0)
+            {
+                this.secondRotSpeed =0;
 
                 //おみくじ機能 rouletteの角度で判断
                 if(transform.localEulerAngles.z < 30.0f || 360.0f < transform.localEulerAngles.z)
                 {
                     Debug.Log("凶");
-                    Debug.Log(transform.localEulerAngles.z);
                 }
                 else if(30.0f <= transform.localEulerAngles.z && transform.localEulerAngles.z < 90.0f)
                 {
                     Debug.Log("大吉");
-                    Debug.Log(transform.localEulerAngles.z);
 
                 }
                 else if(90.0f <= transform.localEulerAngles.z && transform.localEulerAngles.z < 150.0f)
                 {
                     Debug.Log("大凶");
-                    Debug.Log(transform.localEulerAngles.z);
                 }
                 else if(150.0f <= transform.localEulerAngles.z && transform.localEulerAngles.z < 210.0f)
                 {
                     Debug.Log("小吉");
-                    Debug.Log(transform.localEulerAngles.z);
                 }
                 else if(210.0f <= transform.localEulerAngles.z && transform.localEulerAngles.z < 270.0f)
                 {
                     Debug.Log("末吉");
-                    Debug.Log(transform.localEulerAngles.z);
                 }
                 else if(270.0f <= transform.localEulerAngles.z && transform.localEulerAngles.z < 330.0f)
                 {
                     Debug.Log("中吉");
-                    Debug.Log(transform.localEulerAngles.z);
                 }
             }
         }
