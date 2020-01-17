cecd597b21236310a2b404dea0cb6359efc336bf
hiroto302 <i.am.a.hiroto@gmail.com>
2020-01-17 12:06:43 +0900

    AddRotationfunction
    回転機能の追加

---
+++ Assets/Scripts/RouletteController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RouletteController : MonoBehaviour
+{
+    float rotSpeed = 0; //回転スピード
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        //回転機能
+        if(Input.GetMouseButton(0))
+        {
+            this.rotSpeed = 10.0f;
+        }
+        transform.Rotate(0, 0, this.rotSpeed);
+        this.rotSpeed *= 0.98f;
+    }
+}
