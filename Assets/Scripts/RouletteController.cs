5e029ca2c1f311bdee80d6f262bed4625c817f9a
hiroto302 <i.am.a.hiroto@gmail.com>
2020-01-20 12:06:13 +0900

    AddCatAnimation
    猫のアニメーションの追加とテキストの修正

--- Assets/Scripts/RouletteController.cs
+++ Assets/Scripts/RouletteController.cs
@@ -8,8 +8,12 @@ public class RouletteController : MonoBehaviour
 {
     GameObject textBackGround;
     GameObject lotteryResult;
-    float firstRotSpeed = 0; //１度目の回転速度変数
-    float secondRotSpeed= 0; //２度目のタップでの回転速度変数
+    public float firstRotSpeed = 0; //１度目の回転速度変数
+    public float secondRotSpeed= 0; //２度目のタップでの回転速度変数
+
+    //猫オブジェクト取得
+    GameObject zaku;
+    Animator animCtrl;
 
 
 
@@ -19,6 +23,10 @@ public class RouletteController : MonoBehaviour
         lotteryResult = GameObject.Find("LotteryResult");
         this.textBackGround.SetActive(false);
         this.lotteryResult.SetActive(false);
+
+        //猫のアニメーション操作
+        this.zaku = GameObject.Find("zaku-idle0");
+        animCtrl = this.zaku.GetComponent<Animator>();
     }
 
     void Update()
@@ -44,12 +52,15 @@ public class RouletteController : MonoBehaviour
             }
         }
 
+        animCtrl.SetFloat("RotSpeedF", firstRotSpeed);  //猫アニメーション
+
         transform.Rotate(0, 0, this.firstRotSpeed + secondRotSpeed);
         this.secondRotSpeed *= 0.98f;
 
         //２度目のタップで止まる時、くじ結果を表示
         if( 0 < this.secondRotSpeed )
         {
+            animCtrl.SetFloat("RotSpeedF", secondRotSpeed);  //猫アニメーション
             if(this.secondRotSpeed < 0.05f  && firstRotSpeed == 0)
             {
                 this.secondRotSpeed =0;
@@ -60,14 +71,14 @@ public class RouletteController : MonoBehaviour
                     Debug.Log("凶");
                     this.textBackGround.SetActive(true);
                     this.lotteryResult.SetActive(true);
-                    this.lotteryResult.GetComponent<Text>().text= "くじ結果\n凶\nアゲてこ！！";
+                    this.lotteryResult.GetComponent<Text>().text= "くじ結果\n凶\nアゲてくNya！！";
                 }
                 else if(30.0f <= transform.localEulerAngles.z && transform.localEulerAngles.z < 90.0f)
                 {
                     Debug.Log("大吉");
                     this.textBackGround.SetActive(true);
                     this.lotteryResult.SetActive(true);
-                    this.lotteryResult.GetComponent<Text>().text= "くじ結果\n大吉\n最高！！神！";
+                    this.lotteryResult.GetComponent<Text>().text= "くじ結果\n大吉\n最高！！神ニャ！";
 
                 }
                 else if(90.0f <= transform.localEulerAngles.z && transform.localEulerAngles.z < 150.0f)
@@ -75,31 +86,30 @@ public class RouletteController : MonoBehaviour
                     Debug.Log("大凶");
                     this.textBackGround.SetActive(true);
                     this.lotteryResult.SetActive(true);
-                    this.lotteryResult.GetComponent<Text>().text= "くじ結果\n大..\n大吉最高！！";
+                    this.lotteryResult.GetComponent<Text>().text= "くじ結果\n大..\n大吉最高にゃ！！";
                 }
                 else if(150.0f <= transform.localEulerAngles.z && transform.localEulerAngles.z < 210.0f)
                 {
                     Debug.Log("小吉");
                     this.textBackGround.SetActive(true);
                     this.lotteryResult.SetActive(true);
-                    this.lotteryResult.GetComponent<Text>().text= "くじ結果\n小吉\n最高！！";
+                    this.lotteryResult.GetComponent<Text>().text= "くじ結果\n小吉\n最高にゃ！！";
                 }
                 else if(210.0f <= transform.localEulerAngles.z && transform.localEulerAngles.z < 270.0f)
                 {
                     Debug.Log("末吉");
                     this.textBackGround.SetActive(true);
                     this.lotteryResult.SetActive(true);
-                    this.lotteryResult.GetComponent<Text>().text= "くじ結果\n末吉\nアゲてこ！！";
+                    this.lotteryResult.GetComponent<Text>().text= "くじ結果\n末吉\nアゲてくにゃ！！";
                 }
                 else if(270.0f <= transform.localEulerAngles.z && transform.localEulerAngles.z < 330.0f)
                 {
                     Debug.Log("中吉");
                     this.textBackGround.SetActive(true);
                     this.lotteryResult.SetActive(true);
-                    this.lotteryResult.GetComponent<Text>().text= "くじ結果\n中吉\n最高！！";
+                    this.lotteryResult.GetComponent<Text>().text= "くじ結果\n中吉\n最高にゃ！！";
                 }
             }
         }
-
     }
 }
---
+++ Assets/Scripts/ZakuController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZakuController : MonoBehaviour
+{
+    Animator animCtrl;
+    public RouletteController roulette;
+    float zakuFirstRotSpeed ;
+    void Start()
+    {
+        animCtrl = GetComponent<Animator>();
+        zakuFirstRotSpeed = roulette.firstRotSpeed;
+        // zakuFirstRotSpeed = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if(0.5f < zakuFirstRotSpeed)
+        // {
+        //     animCtrl.SetBool("RotSpeed", true);
+        // }
+        // else
+        // {
+        //     animCtrl.SetBool("RotSpeed", false);
+        // }
+
+        // if(Input.GetMouseButtonDown(0))
+        // {
+        //     this.zakuFirstRotSpeed = 10.0f;
+            // animCtrl.SetFloat("RotSpeedF", zakuFirstRotSpeed);
+        // }
+    }
+}
